19f2a4eb977ee9baa8cf39a200aef61660f10039
raaasin <nisarvskp@gmail.com>
2024-06-09 02:19:56 +0530

    updated test

--- test.py
+++ test.py
@@ -4,11 +4,21 @@ import os
 import pathlib
 import google.generativeai as genai
 from dotenv import load_dotenv
-
+from googlesearch import search
 load_dotenv()
 genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
 st.title("Webcam Capture")
+
+def alternative_product(input: str):
+    """returns a string of internet search of best or healthy alternative indian product for the given input product"""
+    input_query = "What is the healthiest " + input + " in India"
+    search_results = search(input_query, num_results=5, advanced=True)
+    results=""
+    for i in search_results:
+        results+=str(i)
+    return results
+
 
 # Add some CSS to make the camera input more responsive
 st.markdown(
@@ -43,11 +53,43 @@ if img_file_buffer is not None:
     }
 
     response = model.generate_content(
-        ["Read all contents of the label, based on all contents rate it out of 5 for edible products based on health and for inedible products like groceries or makeup tell about safe usage etc,reply like this: Rating:, Reason:, expiry if mentioned:", picture],
+        ["Read all contents of the label, based on all contents rate it out of 5 for edible products based on health and for inedible products like groceries or makeup tell about safe usage etc,reply like this: Rating:, Reason:, expiry if mentioned:, reply in json format", picture],
         generation_config=genai.types.GenerationConfig(
             candidate_count=1,
             temperature=0
         )
     )
     response.resolve()
-    st.text_area("Response", response.text)
+    #remove ``` from the response`
+    response = response.text.replace("```", "")
+    response = response.replace("json", "")
+    st.text(response)
+
+    if st.button("Suggest alternative product"):
+        with st.spinner("Identifying Product Name"):
+            response = model.generate_content(
+                ["what is this? just tell the name nothing else", picture],
+                generation_config=genai.types.GenerationConfig(
+                    candidate_count=1,
+                    temperature=0,
+                )
+            )
+            response.resolve()
+            product_name = response.text.strip()
+
+        with st.spinner("Searching the internet"):
+            result = alternative_product(product_name)
+
+        with st.spinner("Almost done!"):
+            prompt = f"the best alternative product is {result} for {product_name} in India, reply like this Alternative:, Reason:, reply in json format"
+            response = model.generate_content(
+                [prompt],
+                generation_config=genai.types.GenerationConfig(
+                    candidate_count=1,
+                    temperature=0,
+                )
+            )
+            response.resolve()
+            response = response.text.replace("```", "")
+            response = response.replace("json", "")
+            st.text(response)
